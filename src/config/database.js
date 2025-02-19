e0853eb203a9300130d50360fb11d7b935553ad6
Dagim Ashenafi <74710537+dagim19@users.noreply.github.com>
2025-02-19 19:10:16 +0300

    added a unified database

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,20 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+const connectToAtlas = async () => {
+  try {
+    const connectionLink = `mongodb+srv://${process.env.ATLAS_USER_NAME}:${process.env.ATLAS_PASSWORD}@cluster0.mshyo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+    await mongoose.connect(connectionLink);
+  } catch (err) {
+    // pass the error to the caller
+    throw err
+  }
+}
+
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    // await mongoose.connect(process.env.MONGODB_URI);
+    await connectToAtlas();
     console.log('MongoDB Connected...');
   } catch (err) {
     console.error(err.message);
