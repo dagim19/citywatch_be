2874b141195100252a37c9fb74616ce7547cb5c6
Amanuelm <75570250+Amanuelm@users.noreply.github.com>
2025-03-06 22:43:44 +0300

    reject

--- src/models/Report.js
+++ src/models/Report.js
@@ -47,7 +47,7 @@ const ReportSchema = new mongoose.Schema({
   },
   status: {
     type: String,
-    enum: ["pending", "verified"],
+    enum: ["pending", "verified","rejected"],
     default: "pending",
   },
   issueType: {
